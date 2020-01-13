0bad549d5ebe408f8dd2d0296b95eee323948eb0
See Yang <seeyang24@gmail.com>
2020-01-13 17:02:08 -0600

    working on validation2

--- ppmtool/src/main/java/io/seeyang/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/seeyang/ppmtool/web/ProjectController.java
@@ -5,10 +5,16 @@ import io.seeyang.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,8 +28,25 @@ public class ProjectController {
     // post mapping entity will return a response entity - return JSON responses and control response statuses
     // method is named createNewProject that takes in a request body of project
     // returns the new project and a status of created
+
+    // @VALID is used to make sure we are passing a valid request
+    // binding result is an interface that invokes the validator on an object - determines if there are errors
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        // if the result has an error, return the list of errors below
+        if (result.hasErrors()) {
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            // extract field and message and then display map with a FOR LOOP
+            for (FieldError error: result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            // return the mapped string with errors
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
         // call the project service method to save or update the passed project
        project = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
