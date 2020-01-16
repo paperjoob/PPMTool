25a030a8de42c1f875acf96a64b5d3920845d97c
See Yang <seeyang24@gmail.com>
2020-01-16 16:29:39 -0600

    worked on adding store.js

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import AddProject from "./components/Project/AddProject";
+import {Provider} from "react-redux"; // allows us to connect react with redux
+import store from "./store";
 
-function App() {
-  return (
-    // wrap the app.js in the router tag to enable us to have specific routes
-    <Router>
-    <div className="App">
-      <Header />
-      <Route exact path="/dashboard" component={Dashboard}/>
-      <Route exact path="/addProject" component={AddProject}/>
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+   return (
+     <Provider store={store}>
+    {/* wrap the app.js in the router tag to enable us to have specific routes */}
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route exact path="/addProject" component={AddProject}/>
+        </div>
+      </Router>
+    </Provider>
+    );
+  }
 }
 
 export default App;
