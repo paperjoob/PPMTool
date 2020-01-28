7f393096492658da6f65412d2f072ad83a4b23c6
See Yang <seeyang24@gmail.com>
2020-01-28 14:24:14 -0600

    able to add tasks in the server and sequences increase

---
+++ ppmtool/src/main/java/io/seeyang/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,50 @@
+package io.seeyang.ppmtool.services;
+
+import io.seeyang.ppmtool.domain.Backlog;
+import io.seeyang.ppmtool.domain.ProjectTask;
+import io.seeyang.ppmtool.repositories.BacklogRepository;
+import io.seeyang.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    // a method that returns a projectTask called addProjectTask
+    // takes two parameters: project identifier and project task
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        // I want Project Tasks to be added to a specific project, make sure project != null = Backlog exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        // Set the backlog to the project task which sets the relationship
+        projectTask.setBacklog(backlog);
+        // We want our project sequence to be like this: IDPRO-1, IDPRO-2, IDPRO-3
+        Integer BackLogSequence = backlog.getPTSequence(); // grab the PT Sequence, which is initialized at 0
+        // Update the Backlog sequence so if you delete 3, the sequence will be 4
+        BackLogSequence++; // increase the sequence right away
+        backlog.setPTSequence(BackLogSequence);
+
+        // Add sequence to the project task and project identifier
+        projectTask.setProjectSequence(projectIdentifier+"-"+BackLogSequence); // EX: IDPRO-3
+        projectTask.setProjectIdentifier(projectIdentifier); // set project identifier
+
+        // Set initial priority, if null
+        // in the future, need to add projectTask.getPriority() == 0 ||
+        if(projectTask.getPriority() == null) {
+            projectTask.setPriority(3); // if there are no priorities set, default it to a 3 - LOW PRIORITY
+        }
+
+        // Set initial status, if null
+        if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO"); // if null, set the status to "TO_DO"
+        }
+
+        // return a saved project task
+        return projectTaskRepository.save(projectTask);
+    }
+}
---
+++ ppmtool/src/main/java/io/seeyang/ppmtool/web/BacklogController.java
@@ -0,0 +1,37 @@
+package io.seeyang.ppmtool.web;
+
+import io.seeyang.ppmtool.domain.ProjectTask;
+import io.seeyang.ppmtool.services.MapValidationErrorService;
+import io.seeyang.ppmtool.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin // needed to cross connect to React
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                                     BindingResult result, @PathVariable String backlog_id) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap; // if there are errors, return the errors
+
+        // new project task that takes the backlog id and project task
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+        // return new project task and the http status created
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+}
